2137268036a20ef036e3fd1cb5868667855a7f8f
xukp20 <xukp20@mails.tsinghua.edu.cn>
2024-06-19 12:04:14 +0000

    update

---
+++ llm/chat.py
@@ -0,0 +1,6 @@
+"""
+    A script for chatting with GLM to do this task:
+    Come up with a sentence to conclude the requirements to be translated into keywords.
+    NOTE: this runs in console, needs a frontend if needed.
+"""
+
